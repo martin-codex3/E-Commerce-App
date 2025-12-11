03b5b16bca21a80ed662b9329a40b3ef86d3ef76
martin-codex3 <martinsilungwe12@gmail.com>
2025-12-11 10:42:19 +0200

    adding database connections

---
+++ Backend/app/config/app_config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+
+# we will attempt to get the environment settings here
+class AppConfig(BaseSettings):
+    SQLITE_DATABASE: str
+
+    # using the settings dict here
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True
+    )
+
+
+# we will create the object for the config class here
+app_config = AppConfig()
---
+++ Backend/app/connection/db_connection.py
@@ -0,0 +1,4 @@
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+
+
+# we will attempt to create the psych engine here
