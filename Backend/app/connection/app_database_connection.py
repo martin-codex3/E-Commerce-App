bbe99451ef8452c602e3cb3cce45bdca0942e174
martin-codex3 <martinsilungwe12@gmail.com>
2025-12-16 16:48:13 +0200

    adding new database connection

--- Backend/app/connection/app_database_connection.py
+++ Backend/app/connection/app_database_connection.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
-
 from app.config.app_config import AppConfig
 from sqlalchemy.ext.asyncio.engine import AsyncEngine
-
+from sqlmodel import SQLModel
 
 # creating the database engine here
 async_database_engine = AsyncEngine(
@@ -14,3 +13,6 @@ async_database_engine = AsyncEngine(
 
 
 # initializing the database here
+async def database_init():
+    async with async_database_engine.begin() as connection:
+        await connection.run_sync(SQLModel.metadata.create_all)
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.routes.products.product_routes import product_router
+from contextlib import asynccontextmanager
+from app.connection.app_database_connection import database_init
+
 
 app_api_version = "v1"
 # the routes for the app
 
-app = FastAPI(version=app_api_version)
+# the application life span here
+@asynccontextmanager
+async def app_life_span(app: FastAPI):
+    await database_init()
+    yield
+    print("We are terminating here")
+
+app = FastAPI(version=app_api_version, lifespan=app_life_span)
+
 
 app.include_router(
     router=product_router,
