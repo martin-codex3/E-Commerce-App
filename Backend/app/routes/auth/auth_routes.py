d649531d052b06ac56d89765a03eee3c316ded0f
martin-codex3 <martinsilungwe12@gmail.com>
2025-12-19 09:43:34 +0200

    adding password hashing algorithm

--- Backend/app/routes/auth/auth_routes.py
+++ Backend/app/routes/auth/auth_routes.py
@@ -11,3 +11,4 @@ auth_services = AuthService()
 @auth_router.get("/", status_code=status.HTTP_201_CREATED)
 async def create_account(user_data: CreateUserSchema, session: AsyncSession):
     """ we first have to check the user here"""
+    
