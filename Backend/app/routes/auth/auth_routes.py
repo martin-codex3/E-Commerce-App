89da6f2ea6478891c0a6cd9a264b4a3ba3457329
martin-codex3 <martinsilungwe12@gmail.com>
2025-12-19 16:01:09 +0200

    working on the user account creation

--- Backend/app/routes/auth/auth_routes.py
+++ Backend/app/routes/auth/auth_routes.py
@@ -7,6 +7,7 @@ from app.connection.app_database_connection import get_app_session
 
 
 auth_router = APIRouter()
+
 auth_services = AuthService()
 
 
@@ -15,20 +16,16 @@ async def create_account(user_data: CreateUserSchema, session: AsyncSession = De
     """ we first have to check the user here"""
     email: EmailStr = user_data.email
 
-    user = await auth_services.check_if_user_exists(
-        email = email,
-        session = session
-    )
+    # checking if the user exists
+    user_exists = await auth_services.check_if_user_exists(email = email, session = session)
 
-    if user is not None:
+    if user_exists:
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail="Email address already taken, try another email")
+
+    else:
         new_user = await auth_services.create_new_user(
             user_data = user_data,
             session = session
         )
 
         return new_user
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Failed to create the user check your details and try again"
-        )
