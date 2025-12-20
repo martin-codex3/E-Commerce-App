10f693e47064bf6379bbd7769a0c08c672d408bf
martin-codex3 <martinsilungwe12@gmail.com>
2025-12-20 12:23:25 +0200

    adding functions to decode the jwt token

--- Backend/app/config/app_config.py
+++ Backend/app/config/app_config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class AppConfig(BaseSettings):
 
     DATABASE_URL: str
+    JWT_ALGORITHM: str
+    JWT_KEY: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
--- Backend/app/utils/jwt_password_management.py
+++ Backend/app/utils/jwt_password_management.py
@@ -1,7 +1,48 @@
-import jwt
-from datetime import timedelta
+import logging
 
+import jwt
+from datetime import timedelta, datetime
+
+from sqlalchemy.sql.coercions import expect
+
+from app.config.app_config import AppConfig
+
+
+jwt_key = AppConfig.JWT_KEY
+jwt_algorithm = AppConfig.JWT_ALGORITHM
 
 # function to encode the jwt token
 def encode_jwt_token(user_data: dict, expiry: timedelta = None, refresh: bool = False) -> str:
-    
+    payload = {
+        "user": user_data,
+        "exp": datetime.now() + (expiry if expiry is not None else timedelta(minutes=60)),
+        "iat": datetime
+    }
+
+    token = jwt.encode(
+        payload = payload,
+        key=jwt_key,
+        algorithm=jwt_algorithm
+    )
+
+    return token
+
+
+# function to decode the toke  here
+def decode_jwt_token(jwt_token: str):
+    try:
+        token = jwt.decode(
+            algorithms=[AppConfig.JWT_ALGORITHM],
+            key=AppConfig.JWT_KEY,
+            jwt=jwt_token
+        )
+
+        return token
+    except jwt.PyJWTError as error:
+        logging.exception(error)
+        return None
+
+    except Exception as e:
+        logging.exception(e)
+        return None
+
