7050622443fa8d4a749c91017bc6afa0e9a8ab3d
martin-codex3 <martinsilungwe12@gmail.com>
2025-12-23 11:10:58 +0200

    adding accepted origins

--- Backend/app/config/app_config.py
+++ Backend/app/config/app_config.py
@@ -6,6 +6,9 @@ class AppConfig(BaseSettings):
     DATABASE_URL: str
     JWT_ALGORITHM: str
     JWT_KEY: str
+    ALLOWED_ORIGIN_ONE: str
+    ALLOWED_ORIGIN_TWO: str
+    ALLOWED_ORIGIN_THREE: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
